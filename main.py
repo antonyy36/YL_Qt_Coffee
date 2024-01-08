f804a95334b1c9a5543cd9ef4d64705daaa72e3d
ANTON <filatov21082007@gmail.com>
2024-01-08 23:01:44 +0300

    commit_coffee

--- main.py
+++ main.py
@@ -10,9 +10,14 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('main.ui', self)
         self.con = sqlite3.connect("coffee.sqlite3")
-        self.adding()
+        self.f = False
+        self.AddButton.clicked.connect(self.adding)
+        self.AddButton.click()
 
     def adding(self):
+        if self.f:
+            self.add_form = AddWidget(self)
+            self.add_form.show()
         if True:
             cur = self.con.cursor()
             self.result = cur.execute("SELECT * FROM coffee").fetchall()
@@ -29,6 +34,44 @@ class MyWidget(QMainWindow):
                 for j, elem in enumerate(row):
                     self.tableWidget.setItem(
                         i, j, QTableWidgetItem(str(elem)))
+            self.f = True
+
+    def save_result(self, type, obj, mol_zern, taste, price, vol):
+        print(type, obj, mol_zern, taste, price, vol)
+        cur = self.con.cursor()
+        cur.execute("INSERT INTO coffee VALUES(NULL, ?, ?, ?, ?, ?, ?)",
+                    (type, obj, mol_zern, taste, price, vol,))
+        self.con.commit()
+        self.f = False
+        self.AddButton.click()
+
+
+class AddWidget(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.parent1 = parent
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.SaveButton.clicked.connect(self.get_adding_verdict)
+
+    def get_adding_verdict(self):
+        if self.lineEdit.text() == '' or self.plainTextEdit.toPlainText() == '':
+            self.statusBar().showMessage('Неверно заполнена форма')
+            return False
+        else:
+            text = ''
+
+            self.statusBar().showMessage('')
+
+            if self.radioButton.isChecked():
+                text = self.radioButton.text()
+            elif self.radioButton_2.isChecked():
+                text = self.radioButton_2.text()
+            self.parent1.save_result(str(self.lineEdit.text()), self.spinBox.value(),
+                                     str(text), str(self.plainTextEdit.toPlainText()), self.doubleSpinBox.value(),
+                                     self.spinBox_2.value())
+
+            self.close()
+            return True
 
 
 if __name__ == '__main__':
